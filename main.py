600ab7de745767b81e6f308b15dcc44ad27248fe
taranek98 <84998904+taranek98@users.noreply.github.com>
2024-08-27 15:17:25 +0200

    Add New element function

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
 from tkinter import *
 
+def addNewElement(title):
+   global currentIndex
+   currentIndex = currentIndex + 1
+   list_wish.insert(currentIndex, title)
+
+def newElementLoop():
+    
+    def closeWindow():
+        addNewElement(entry_title.get())
+        newWindow.destroy()
+
+    newWindow = Toplevel(window)        
+    newWindow.title('Dodaj nową grę')
+    newWindow.geometry("400x130")
+    newWindow.config(background='#312d2d')
+
+    entry_title = Entry(newWindow, width = 40)
+    entry_title.place(x = 100, y = 25)
+
+    button_accept = Button(newWindow, text='Zatwierdź', width=10, height=2, command=closeWindow)
+    button_accept.place(x = 160, y = 75)
+
+    label_title = Label(newWindow, text="Tytuł:", background = '#312d2d', foreground= "white", font=('Arial',16))
+    label_title.place(x = 40, y = 20)
+
+currentIndex = 2 #set to last element list_wish
 window = Tk()
 window.title('Dream List')
 window.geometry("700x500")
 window.config(background='#312d2d')
 
 
-button_addElement = Button(window, text='Dodaj grę', width=15, height= 2)
+button_addElement = Button(window, text='Dodaj grę', width=15, height= 2, command=newElementLoop)
 button_addElement.place(x = 290, y = 420)
 
-label_wishListTitle = Label(window, text="Wish List")
+label_wishListTitle = Label(window, text="Lista życzeń")
 label_wishListTitle.place(x = 490, y = 20)
 
 list_wish = Listbox(window, height=20, width=40)
 list_wish.place(x = 400, y = 50)
 list_wish.insert(0, 'The Legend of Zelda Tears of the Kingdom')
-list_wish.insert(5, "Pokemom Let's go Pikachu")
-list_wish.insert(2, 'Dark Souls Remaster')
+list_wish.insert(2, "Pokemom Let's go Pikachu")
+list_wish.insert(1, 'Dark Souls Remaster')
 
-label_finishListTitle = Label(window, text="Finish Game List")
+label_finishListTitle = Label(window, text="Lista ukończonych gier")
 label_finishListTitle.place(x = 130, y = 20)
 
 list_finish = Listbox(window, height=20, width=40)
 list_finish.place(x = 60, y = 50)
 list_finish.insert(0, 'The Legend of Zelda Breath of the Wild')
+
 
 window.mainloop()
