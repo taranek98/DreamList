54ae7cb9e478d9e1ef50174677f759b7f9493703
taranek98 <84998904+taranek98@users.noreply.github.com>
2024-08-29 10:28:17 +0200

    Mouse repair 0.01

--- main.py
+++ main.py
@@ -27,14 +27,17 @@ def newElementWindow():
     label_title.place(x = 40, y = 20)
 
 def defineCurrentSelectElement(list):
-    global currentSelectElement
-    currentSelectElement = list.curselection()[0]
-
-def moveElementOnList(moveIsUp, list):
     if(len(list.curselection()) != 0): #check is element of list is selected
-        if(list.curselection()[0] != 0 or moveIsUp == False): #check is element can move up
-            if(list.curselection()[0] < list.size()-1 or moveIsUp == True): #check is element can move down
-                currentSelectElement = list.curselection()[0]
+        global currentSelectElement
+        currentSelectElement = list.curselection()[0]
+
+def moveElementOnList(moveIsUp, list, isMouse):
+    if(len(list.curselection()) != 0): #check is element of list is selected
+        global currentSelectElement
+        if(isMouse == False):
+            currentSelectElement = list.curselection()[0]
+        if(currentSelectElement != 0 or moveIsUp == False): #check is element can move up
+            if(currentSelectElement < list.size()-1 or moveIsUp == True): #check is element can move down
                 var = list.get(currentSelectElement)
                 list.delete(currentSelectElement)
                 if(moveIsUp == True):
@@ -44,14 +47,18 @@ def moveElementOnList(moveIsUp, list):
                 list.activate(currentSelectElement)
 
 def moveElementByMouse(list):
-    if(len(list.curselection()) != 0): #check is element of list is selected
-        global currentSelectElement
-        print(list.curselection()[0])
-        newSelection = list.curselection()[0]
-        if(currentSelectElement > newSelection):
-            moveElementOnList(False, list)
-        elif(currentSelectElement < list.curselection()[0]):
-            moveElementOnList(True, list)
+    # if(len(list.curselection()) != 0): #check is element of list is selected
+        # global currentSelectElement
+        # print(list.curselection()[0])
+        # newSelection = list.curselection()[0]
+        # if(currentSelectElement > newSelection):
+        #     moveElementOnList(True, list, True)
+        #     currentSelectElement = newSelection
+        # elif(currentSelectElement < list.curselection()[0]):
+        #     moveElementOnList(False, list, True)
+        #     currentSelectElement = newSelection
+        # list.activate(newSelection)
+    print(list.nearest())
         
         
 
@@ -73,8 +80,8 @@ list_wish.place(x = 400, y = 50)
 list_wish.insert(0, 'The Legend of Zelda Tears of the Kingdom')
 list_wish.insert(2, "Pokemom Let's go Pikachu")
 list_wish.insert(1, 'Dark Souls Remaster')
-list_wish.bind('<Up>', lambda event:moveElementOnList(True, list_wish))
-list_wish.bind('<Down>', lambda event:moveElementOnList(False, list_wish))
+list_wish.bind('<Up>', lambda event:moveElementOnList(True, list_wish, False))
+list_wish.bind('<Down>', lambda event:moveElementOnList(False, list_wish, False))
 list_wish.bind('<B1-Motion>', lambda event:moveElementByMouse(list_wish))
 list_wish.bind('<FocusIn>', lambda event:defineCurrentSelectElement(list_wish))
 currentSelectElement = -1
@@ -88,8 +95,8 @@ list_finish.insert(0, 'The Legend of Zelda Breath of the Wild')
 list_finish.insert(1, '1111111111')
 list_finish.insert(2, '2222222222')
 list_finish.insert(3, '33333333')
-list_finish.bind('<Up>', lambda event:moveElementOnList(True, list_finish))
-list_finish.bind('<Down>', lambda event:moveElementOnList(False, list_finish))
+list_finish.bind('<Up>', lambda event:moveElementOnList(True, list_finish, False))
+list_finish.bind('<Down>', lambda event:moveElementOnList(False, list_finish, False))
 list_finish.bind('<B1-Motion>', lambda event:moveElementByMouse(list_finish))
 list_finish.bind('<FocusIn>', lambda event:defineCurrentSelectElement(list_finish))
 
